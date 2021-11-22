cbb952406181e15da6e6e4f650d10f516256b451
alexia-sibner <alexia@sibnerpartnership.co.uk>
2021-11-22 17:03:16 +0000

    Added imports to the MyLibService and displayed get name service method on app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { MyLibService } from 'my-lib';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'main-project';
+  constructor(private myLibService: MyLibService) {}
+
+  ngOnInit() {
+    console.log(this.myLibService.getNameService())
+  }
+
+
 }
